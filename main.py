d727ae225850eb6fbb949287a3256a189d39fbf8
amir <hawxfly@gmail.com>
2020-09-19 23:12:33 +0430

    added value with unit

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ time.sleep(10)
 # ---------------------- product detail ( جزییات محصولات ) ----------------
 product_name_xpath = '//*[@id="product_modify_title_control_farsi"]'
 product_name = 'امیرحسین'
-s.driver.ensure_element_by_xpath(product_name_xpath, timeout=10).send_keys(product_name)
+s.driver.ensure_element_by_xpath(product_name_xpath, timeout=30).send_keys(product_name)
 
 open_category_menu = '//*[@id="page_content_inner"]/div/div[2]/div[1]/div[2]/div/div[1]/div[3]/div/div[1]'
 s.driver.ensure_element_by_xpath(open_category_menu,
                                  timeout=10).ensure_click()
-select_category = '//*[@id="page_content_inner"]/div/div[2]/div[1]/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/div[11]'
+select_category = '//*[@id="page_content_inner"]/div/div[2]/div[1]/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/div[96]'
 s.driver.ensure_element_by_xpath(select_category,
                                  timeout=10).ensure_click()
 
@@ -87,5 +87,46 @@ inventory_xpath = '//*[@id="product_modify_quantity_control"]'
 inventory_value = '1000'
 s.driver.ensure_element_by_xpath(inventory_xpath, timeout=10).send_keys(inventory_value)
 
+# -------------------- product filter and data --------------------
 
+# ***************  simple text with predefine value ****************
+text_menu_xpath = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div/div[1]'
+s.driver.ensure_element_by_xpath(text_menu_xpath,
+                                 timeout=10).ensure_click()
+# text_xpath = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]'
+# s.driver.ensure_element_by_xpath(text_xpath,
+#                                  timeout=10).ensure_click()
+match_text = '865'
+try:
+    text_xpath = '//*[text()="{temp}"]'.format(temp=match_text)
+    print(text_xpath)
+    s.driver.ensure_element_by_xpath(text_xpath,
+                                     timeout=10).ensure_click()
+    print('no predefine value')
+except:
+    add_text_xpath = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div/div[2]/div/div'
+    text_input = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div/div[1]/input'
+    s.driver.ensure_element_by_xpath(text_input, timeout=10).send_keys(match_text)
+    time.sleep(0.5)
+    s.driver.ensure_element_by_xpath(add_text_xpath,
+                                     timeout=10).ensure_click()
+    print('new value added')
+
+# ********************* value with unit *******************
+
+value_input_xpath = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[5]/div/div[2]/div/div/div[1]/div/input'
+value_input = '25'
+s.driver.ensure_element_by_xpath(value_input_xpath, timeout=10).send_keys(value_input)
+
+unit_menu_xpath = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[5]/div/div[2]/div/div/div[2]/select'
+unit_value = 'mm'
+unit_xpath = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[5]/div/div[2]' \
+             '/div/div/div[2]/select/option[contains(text(),"{temp}")]'.format(temp=unit_value)
+print(unit_xpath)
+s.driver.ensure_element_by_xpath(unit_menu_xpath,
+                                 timeout=10).ensure_click()
+time.sleep(0.5)
+temp_unit = '//*[@id="page_content_inner"]/div/div[2]/div[3]/div[5]/div/div[2]/div/div/div[2]/select/option[2]'
+s.driver.ensure_element_by_xpath(unit_xpath,
+                                 timeout=10).ensure_click()
 
